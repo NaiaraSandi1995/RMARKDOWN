8f66462d57d0487acb1277f0cb28290d8a76cc7e
Naiara Sandi de Almeida Alcantara <63126159+NaiaraSandi1995@users.noreply.github.com>
2022-12-01 13:11:02 -0300

    Add files via upload

---
+++ código_de_teste.R
@@ -0,0 +1,61 @@
+---
+title: Produto 9.  Relatório  por  Ramo  de  Atividade  comercial  ou  de  serviços – Ramo  de Atividade 1 
+author:
+- 'Analista: Naiara Sandi de Almeida Alcantara'
+- 'Estagiário: Paulo Henrique Souza de Oliveira'
+output:
+  word_document:
+    number_sections: yes
+  officedown::rdocx_document:
+    tables:
+      style: Table
+      layout: autofit
+      width: 1.0
+      caption:
+       style: Table Caption
+       pre: 'Table '
+       sep: ': '
+      conditional:
+       first_row: true
+       first_column: false
+       last_row: false
+       last_column: false
+       no_hband: false
+       no_vband: true
+...
+
+
+<center>
+---          
+Relatório vinculado ao Contrato Nº ECE-DSS-4604/2022 - Pesquisa de Posses e Hábitos de Uso de Equipamentos Elétricos na Classe Comercial e de Serviços  
+    
+  
+
+---
+</center>
+
+<br/>
+<br/>
+
+
+
+
+
+<br/>
+
+
+<center>`r format(Sys.Date(),"%d de %B de %Y")`</center>
+
+
+
+# Introdução {.tabset .tabset-fade}
+
+Cada ramo de atividade comercial ou de serviços terá seu relatório específico individual.Este relatório refere-se ao Ramo de Atividade 1. Este relatório tratará deste ramo de atividade apresentando seus resultados pela distribuição da amostra definida pela CONTRATADA, por regiões geográficas e Brasil. De forma a permitir cruzar os dados da distribuição da amostra com os dados da região geográfica e das demais regiões, os dados entre as regiões geográficas e os dados em relação ao Brasil.
+
+## Objetivo: 
+Comparar e estabelecer relações, do mesmo tema, nas diferentes localidades. Por exemplo, a posse de refrigeradores da região Norte, segundo a distribuição da amostra, deverá ser comparada com a posse de refrigeradores das demais regiões (nordeste, centro-oeste, sul e sudeste) e da posse no âmbito nacional, essa regra vale para todos os equipamentos.Portanto o  relatório  deverá  cruzar os  principais  resultados  consolidados no  âmbito nacional com os consolidados no âmbito regional (norte, centro-oeste, sudeste, sul e nordeste), com o objetivo de poder comparar e estabelecer relações, do mesmo tema, nas diferentes escalas espaciais.O  relatório objetiva:  1)  Oferecer  uma  visão  clara  e  atual  da  posse  e  hábitos  de  uso  dos equipamentos  elétricos  na  classe  comercial  ou  de  serviços  deste  ramo  de  atividade;  2) Destacar os hábitos de consumo dos estabelecimentos comerciais ou de serviços deste ramo de  atividade,  apresentando  os  dados  desagregados  de  consumo  de  cada  equipamento pesquisado, avaliando os dados por distribuição da amostra, o nível de conscientização sobre o uso eficiente da energia elétrica, entre outros aspectos.
+
+\vspace{5truemm} 
+
+## Resultados: 
+Os  principais  resultados  devem  ser  apresentados  na  forma  de  tabelas  e  textos,  sempre comparando a informação por distribuição da amostra, região e Brasil.Todos  os  dados  processados  utilizados  no  relatório,  deverão  ser  disponibilizados  pela CONTRATADA para a CONTRATANTE, em formato digital compatível com o aplicativo Excel e com o aplicativo software IBM SPSS.
